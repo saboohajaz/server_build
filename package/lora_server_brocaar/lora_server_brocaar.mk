00cbc9a5d6c0e56fd1f93de437ff9292c7b0fe23
Nandan Chaturbhuj <nandan.chaturbhuj@fleet.space>
2018-03-09 15:17:02 +1030

    Lora App Server

--- package/lora_server_brocaar/lora_server_brocaar.mk
+++ package/lora_server_brocaar/lora_server_brocaar.mk
@@ -9,37 +9,52 @@ LORA_SERVER_BROCAAR_DEPENDENCIES = mosquitto
 LORA_SERVER_BROCAAR_DEPENDENCIES += redis
 LORA_SERVER_BROCAAR_DEPENDENCIES += postgresql
 LORA_SERVER_BROCAAR_DEPENDENCIES += host-go_fleet
+LORA_SERVER_BROCAAR_DEPENDENCIES += host-nodejs
 
 # Build the source
 define LORA_SERVER_BROCAAR_BUILD_CMDS
-	# @echo "Brocaar Lora App Server : downloading"
-	# @GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u github.com/brocaar/lora-app-server || true
-	# @echo "Brocaar Lora App Server : building requirements"
-	# GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make requirements -C \
-	#	$(@D)/src/github.com/brocaar/lora-app-server/
-	# GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make ui-requirements -C \
-	#	$(@D)/src/github.com/brocaar/lora-app-server/
-	# @echo "Brocaar Lora App Server : building executable"
-	# GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin/linux_arm:$(@D)/bin \
-	#	GOOS=linux GOARCH=arm make build -C $(@D)/src/github.com/brocaar/lora-app-server/
-	# @echo "Brocaar Lora App Server : done"
+	@echo "Brocaar Lora App Server : downloading"
+	@GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u github.com/brocaar/lora-app-server || true
+	cp $(BR2_EXTERNAL_PORTAL_PATH)/package/lora_server_brocaar/Makefile \
+		$(@D)/src/github.com/brocaar/lora-app-server/Makefile
+	@cd $(@D)/src/github.com/brocaar/lora-app-server/ui && $(NPM) install
+	@cd $(@D)/src/github.com/brocaar/lora-app-server/ui && $(NPM) run build
+	@echo "Brocaar Lora App Server : building requirements"
+	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make ui-requirements -C \
+		$(@D)/src/github.com/brocaar/lora-app-server/
+	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make requirements -C \
+		$(@D)/src/github.com/brocaar/lora-app-server/
+
+	@echo "Brocaar Lora App Server : building executable"
+	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin/linux_arm:$(@D)/bin \
+		GOOS=linux GOARCH=arm make build -C $(@D)/src/github.com/brocaar/lora-app-server/
+
+	@echo "Brocaar Lora App Server : done"
+
 	@echo "Brocaar Lora Gateway Bridge : downloading"
 	@GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u github.com/brocaar/lora-gateway-bridge || true
+
 	@echo "Brocaar Lora Gateway Bridge : building requirements"
 	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make requirements -C \
 		$(@D)/src/github.com/brocaar/lora-gateway-bridge/
+
 	@echo "Brocaar Lora Gateway Bridge : building executable"
 	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin/linux_arm:$(@D)/bin \
 		GOOS=linux GOARCH=arm make build -C $(@D)/src/github.com/brocaar/lora-gateway-bridge/
+
 	@echo "Brocaar Lora Gateway Bridge : done"
+
 	@echo "Brocaar Lora Server : downloading"
 	@GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u github.com/brocaar/loraserver || true
+
 	@echo "Brocaar Lora Server : building requirements"
 	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin make requirements -C \
 		$(@D)/src/github.com/brocaar/loraserver/
+
 	@echo "Brocaar Lora Server : building executable"
 	GOPATH=$(@D) PATH=$(PATH):$(HOST_GO_FLEET_ROOT)/bin:$(@D)/bin/linux_arm:$(@D)/bin \
 		GOOS=linux GOARCH=arm make build -C $(@D)/src/github.com/brocaar/loraserver/
+
 	@echo "Brocaar Lora Server : done"
 endef
 
@@ -61,14 +76,14 @@ define LORA_SERVER_BROCAAR_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/lib/loraserver/scripts/loraserver.service
 	$(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/loraserver/packaging/deb/logrotate \
 		$(TARGET_DIR)/etc/logrotate.d/loraserver
-	# $(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/build/lora-app-server \
-	#	$(TARGET_DIR)/usr/bin/lora-app-server
-	# $(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/init.sh \
-	#	$(TARGET_DIR)/usr/lib/lora-app-server/scripts/init.sh
-	# $(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/lora-app-server.service \
-	#	$(TARGET_DIR)/usr/lib/lora-app-server/scripts/lora-app-server.service
-	# $(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/logrotate \
-	#	$(TARGET_DIR)/etc/logrotate.d/lora-app-server
+	$(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/build/lora-app-server \
+		$(TARGET_DIR)/usr/bin/lora-app-server
+	$(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/init.sh \
+		$(TARGET_DIR)/usr/lib/lora-app-server/scripts/init.sh
+	$(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/lora-app-server.service \
+		$(TARGET_DIR)/usr/lib/lora-app-server/scripts/lora-app-server.service
+	$(INSTALL) -D -m 0755 $(@D)/src/github.com/brocaar/lora-app-server/packaging/deb/logrotate \
+		$(TARGET_DIR)/etc/logrotate.d/lora-app-server
 endef
 
 $(eval $(generic-package))
