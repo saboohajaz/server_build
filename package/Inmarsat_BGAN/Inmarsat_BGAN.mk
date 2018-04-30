b77c820040e0b6ae5caec002f81f04cabe262b2e
Nandan Chaturbhuj <nandan.chaturbhuj@fleet.space>
2018-04-30 16:51:21 +0930

    Inmarsat BGAN driver

---
+++ package/Inmarsat_BGAN/Inmarsat_BGAN.mk
@@ -0,0 +1,22 @@
+###############################################
+#
+# Inmarsat BGAN
+#
+###############################################
+
+INMARSAT_BGAN_DEPENDENCIES = rp-pppoe
+INMARSAT_BGAN_DEPENDENCIES += pppd
+INMARSAT_BGAN_DR = $(BR2_EXTERNAL_PORTAL_PATH)/package/Inmarsat_BGAN
+
+# Install the application into the rootfs file system
+define INMARSAT_BGAN_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/sbin/inmarsat_bgan/ 
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/inmarsat_bgan_setup $(TARGET_DIR)/usr/sbin/inmarsat_bgan/ 
+	mkdir -p $(TARGET_DIR)/etc/ppp/peers/
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/chap-secrets $(TARGET_DIR)/etc/ppp/
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/pap-secrets $(TARGET_DIR)/etc/ppp/
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/chap-secrets $(TARGET_DIR)/etc/ppp/
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/resolv.conf $(TARGET_DIR)/etc/ppp/peers/ 
+endef
+
+$(eval $(generic-package))
