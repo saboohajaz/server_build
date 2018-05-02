c099242d3582db7a15a1c332147e1f406dfee088
Nandan Chaturbhuj <nandan.chaturbhuj@fleet.space>
2018-05-02 13:00:54 +0930

    Using MQTT to send via BGAN

--- package/Inmarsat_BGAN/Inmarsat_BGAN.mk
+++ package/Inmarsat_BGAN/Inmarsat_BGAN.mk
@@ -12,6 +12,8 @@ INMARSAT_BGAN_DR = $(BR2_EXTERNAL_PORTAL_PATH)/package/Inmarsat_BGAN
 define INMARSAT_BGAN_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/opt/Inmarsat/ 
 	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/inmarsat_bgan_setup $(TARGET_DIR)/opt/Inmarsat/ 
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/IBGAN $(TARGET_DIR)/opt/Inmarsat/ 
+	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/IBGAN_SEND $(TARGET_DIR)/usr/sbin/ 
 	mkdir -p $(TARGET_DIR)/etc/ppp/peers/
 	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/chap-secrets $(TARGET_DIR)/etc/ppp/
 	$(INSTALL) -D -m 0755 $(INMARSAT_BGAN_DR)/pap-secrets $(TARGET_DIR)/etc/ppp/
