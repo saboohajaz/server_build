601d679ca22604dc725beedee9e40b367830ccfb
Nandan Chaturbhuj <nandan.chaturbhuj@fleet.space>
2018-05-02 16:51:53 +0930

    Logging packet forwarder

--- package/packet_forwarder/packet_forwarder.mk
+++ package/packet_forwarder/packet_forwarder.mk
@@ -9,6 +9,8 @@ PACKET_FORWARDER_SITE = https://github.com/Lora-net/packet_forwarder/archive/v$(
 PACKET_FORWARDER_SITE_INSTALL_STAGING = YES
 PACKET_FORWARDER_DEPENDENCIES = loragateway
 PACKET_FORWARDER_OLD_ID := AA555A0000000000
+PACKET_FORWARDER_DR = $(BR2_EXTERNAL_PORTAL_PATH)/package/packet_forwarder
+
 # Build the source
 define PACKET_FORWARDER_BUILD_CMDS
 	make CROSS_COMPILE=$(TARGET_CROSS) ARCH=armv7l -C $(@D)
@@ -27,6 +29,8 @@ define PACKET_FORWARDER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/packet_forwarder/ 
 	cp -R $(@D)/* $(TARGET_DIR)/usr/bin/packet_forwarder/
 	echo $(BR2_PACKAGE_PACKET_FORWARDER_PIN_NUMBER) > $(TARGET_DIR)/usr/bin/packet_forwarder/pin
+	$(INSTALL) -D -m 0755 $(PACKET_FORWARDER_DR)/PacketFwd $(TARGET_DIR)/etc/init.d/S80PacketFwd
+	$(INSTALL) -D -m 0755 $(PACKET_FORWARDER_DR)/LoraPkt $(TARGET_DIR)/usr/bin/packet_forwarder/LoraPkt
 endef
 
 $(eval $(generic-package))
