5145fa7ec495baecd68a0f1928255e38aeb11936
Nandan Chaturbhuj <nandan.chaturbhuj@fleet.space>
2018-05-04 14:12:57 +0930

    Build Portal Engine

--- package/portal_engine/portal_engine.mk
+++ package/portal_engine/portal_engine.mk
@@ -4,23 +4,24 @@
 #
 ###############################################
 
-#PORTAL_ENGINE_VERSION = master
-#PORTAL_ENGINE_SITE = https://bitbucket.org/fleetspace/nano-analytics
-#PORTAL_ENGINE_SOURCE = portal_engine-$(PORTAL_ENGINE_VERSION).tar.gz
-#PORTAL_ENGINE_SITE_METHOD = git
-
 PORTAL_ENGINE_DEPENDENCIES += host-go_fleet
-PORTAL_ENGINE_SRC_DIR = $(@D)/src/bitbucket.org/fleetspace/nano-analytics/
-
+PORTAL_ENGINE_DEPENDENCIES += lora_server_brocaar
+PORTAL_ENGINE_SRC_PATH = bitbucket.org/fleetspace/nano-analytics
+PORTAL_ENGINE_SRC_DIR = $(@D)/src/$(PORTAL_ENGINE_SRC_PATH)/
+PORTAL_ENGINE_INIT_LVL = $(shell echo $$(( $(BR2_PACKAGE_LORA_SERVER_BROCAAR_INIT_LVL) + 1 )))
+PORTAL_ENGINE_DR = $(BR2_EXTERNAL_PORTAL_PATH)/package/portal_engine
 
 define PORTAL_ENGINE_BUILD_CMDS
 	@echo "Build Portal Engine"
-	GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u bitbucket.org/fleetspace/nano-analytics || true
-	cd $(PORTAL_ENGINE_SRC_DIR) && GOPATH=$(PORTAL_ENGINE_SRC_DIR) GOOS=linux GOARCH=arm $(HOST_GO_FLEET_ROOT)/bin/go build -o PortalEngine main.go
+	GOPATH=$(@D) $(HOST_GO_FLEET_ROOT)/bin/go get -u $(PORTAL_ENGINE_SRC_PATH) || true
+	cd $(PORTAL_ENGINE_SRC_DIR) && GOPATH=$(@D) GOOS=linux GOARCH=arm $(HOST_GO_FLEET_ROOT)/bin/go build -o PortalEngine main.go
 endef
 
 define PORTAL_ENGINE_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/opt/PortalEngine/ 
+	mkdir -p $(TARGET_DIR)/opt/PortalEngine/
+	$(INSTALL) -D -m 0755 $(PORTAL_ENGINE_SRC_DIR)/PortalEngine $(TARGET_DIR)/opt/PortalEngine/
+	pwd
+	$(INSTALL) -D -m 0755 $(PORTAL_ENGINE_DR)/PortalEngineService $(TARGET_DIR)/etc/init.d/S$(PORTAL_ENGINE_INIT_LVL)PortalEngineService
 endef
 
 $(eval $(generic-package))
